ea0135c5768fcbc3256ec841e450b0985d57c8af
Marília Santos <90049157+MariliaOlivira@users.noreply.github.com>
2022-04-22 16:45:24 -0300

    Modificações da interface

--- Front/Interface_huff.py
+++ Front/Interface_huff.py
@@ -16,29 +16,32 @@ Tela_principal = Tk()
 Tela_principal.title("Algoritmo de Huffman")
 
 # Tamanho da tela que se adequa a imagem.
-Tela_principal.geometry('300x254+200+200')
+Tela_principal.geometry('340x254+200+200')
 
 # Para evitar que minha janela se redimensione
 Tela_principal.resizable(False, False)
 
+Tela_principal["bg"] = "white"
+
+# Mudando o ícone da tela
+Tela_principal.iconbitmap('imagem/icon.ico')
+
 # Variavel que armazenará a imagem projetada na tela inicial.
-img_principal = PhotoImage(file='imagem/arvoreOfc-removebg-preview.png')
+img_principal = PhotoImage(file='imagem/tree.png')
+img_logo = PhotoImage(file='imagem/logo.png')
+
 
 # Colocação da inagem da tela
-label_image = Label(Tela_principal, image=img_principal).place(x=-1, y=-1)
+label_image = Label(Tela_principal, image=img_principal, bg="white").place(x=25, y=30)
+label_logo = Label(Tela_principal, image=img_logo, bg="white").place(x=0, y=166)
 
 # Importação da imagem
-img_compactar = PhotoImage(file='imagem/imagemDescompactar-removebg-preview.png')
+img_compactar = PhotoImage(file='imagem/img2.png')
+img_descompactar = PhotoImage(file='imagem/pimenta.png')
 
 # bot�es responsaveis pela execu��o do codigo.
-botão_compactar = Button(Tela_principal, text="compactar", image= img_compactar, command=reconstruir )
-botão_descompactar = Button(Tela_principal, text="descompactar", command=compactar)
-botão_reconstruir = Button(Tela_principal, text="reconstruir", command=descompactar)
-
-# Colocando a localização do botão na tela.
-botão_compactar.place(x=280, y=260)
-botão_descompactar.place(x=280, y=180)
-botão_reconstruir.place(x=280, y=220)
+botão_compactar = Button(Tela_principal, text="compactar",image=img_compactar, bg= "white", command=reconstruir).place(x=220, y=50)
+botão_descompactar = Button(Tela_principal, text="descompactar",image=img_descompactar, command=compactar).place(x=220, y=120)
 
 
 # Comando necess�rio para manter a tela "rodando", e tbm responsavel pelo fechamento.
